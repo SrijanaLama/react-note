c535aa41f33c487a85347ba397c446af610684af
SrijanaLama <srijanalama94@gmail.com>
2020-07-26 18:22:32 +0545

    Updates

---
+++ precache-manifest.ea9986b58dd75032d3a495710d8bb00f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5102bc1b48af7b0e70abcc0c4a96ca8f",
+    "url": "/react-note/index.html"
+  },
+  {
+    "revision": "4367f950e56a5e8462b8",
+    "url": "/react-note/static/css/main.ea66f636.chunk.css"
+  },
+  {
+    "revision": "ecbf607d592b57896e61",
+    "url": "/react-note/static/js/2.61866661.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-note/static/js/2.61866661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4367f950e56a5e8462b8",
+    "url": "/react-note/static/js/main.435d5f57.chunk.js"
+  },
+  {
+    "revision": "237dcb4987f43bd26904",
+    "url": "/react-note/static/js/runtime-main.e96beb65.js"
+  }
+]);
