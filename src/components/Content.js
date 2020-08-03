870bb3590796ff42b42590da868da0de67ae9687
SrijanaLama <srijanalama94@gmail.com>
2020-08-03 18:26:51 +0545

    added update logic

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,10 +7,23 @@ import Note from "./Note"
 class Content extends React.Component {
     state = {
         id: 0,
+        tempId: 0,
         subject: " ",
         description: " ",
         myNoteArray: []
     }
+    handleUpdate = (inputid) =>{
+    let  myNote= JSON.parse(sessionStorage.getItem("myNote"));
+    this.setState(prevState =>({
+        tempId : prevState.id,
+        id: Number(inputid),
+        subject:  myNote[inputid].subject,
+        description: myNote[inputid].description
+    }))
+    
+   
+
+}
 
     handleChange = (event) => {
         const { name, value } = event.target
@@ -25,30 +38,48 @@ class Content extends React.Component {
         
         const { value } = event.target
         if (value === "add") {
-            this.setState(prevState => ({
-                myNoteArray: [...prevState.myNoteArray, {
-                    'id': prevState.id,
-                    'subject': prevState.subject,
-                    'description': prevState.description
-                }],
-                id: prevState.id + 1,
-                subject: " ",
-                description: " "
+            //check if id already existed
+            if(this.state.id < this.state.myNoteArray.length){
+                  this.setState(prevState =>({
+                    myNoteArray: prevState.myNoteArray.map((note, index) =>
+                    index === prevState.id ? { ...note, 'subject': prevState.subject,
+                    'description': prevState.description } : note,
+                 ),
+                    id : prevState.tempId,
+                    tempId: 0,
+                    subject:  "",
+                    description: ""
+                }))
 
-            }))
-             alert("Added");
+            }
+            else{
+                this.setState(prevState => ({
+                    myNoteArray: [...prevState.myNoteArray, {
+                        'id': prevState.id,
+                        'subject': prevState.subject,
+                        'description': prevState.description
+                    }],
+                    id: prevState.id + 1,
+                    subject: " ",
+                    description: " "
+    
+                }))
+
+            }
+                    
         }
+        //case for clear
         else {
             this.setState({
                 subject: " ",
                 description: " "
             })
         }
-        
-
+          alert("Added" );
     }
 
     render() {
+        sessionStorage.setItem("myNote",  JSON.stringify(this.state.myNoteArray));
         return (
             <div className="mainContainer">
 
@@ -57,7 +88,7 @@ class Content extends React.Component {
                     <ContentManage sub={this.state.subject} desc={this.state.description} updateValue={this.handleChange} />
 
                 </div>
-                <Note myNoteList={this.state.myNoteArray} />
+                <Note myNoteList={this.state.myNoteArray}  onUpdate={this.handleUpdate}/>
 
 
             </div>
--- src/components/Note.js
+++ src/components/Note.js
@@ -3,18 +3,29 @@ import '../index.css'
 import { FaTrash } from "react-icons/fa"
 
 const Note = props => {
+    const handleClick = (e) =>{
+        props.onUpdate(e.currentTarget.id);
+       
+    }
+    const handleDelete = (e) =>{
+        if (window.confirm("Delete the item?")) {
+        //Logic to delete the item
+        var element = document.getElementById(e.currentTarget.id);
+         element.parentNode.removeChild(element);
+    }
+    }
     return (
         <div className="note">
             <h2>Notelist</h2>
             {
                 props.myNoteList.map(note => {
                     return (
-                        <div key={note.id} id={note.id} className="noteList">
+                        <div key={note.id} id={note.id} className="noteList" onClick={handleClick}>
                             <li className="list">
                                 <h1 hidden>  {note.id} </h1>
                                 <h3 className="subNote"> {note.subject}</h3>
                             </li>
-                            <button onClick={() => handleDelete(note.id)} className="deleteBtn"> <FaTrash /> </button>
+                            <button id={note.id} onClick={handleDelete} className="deleteBtn"> <FaTrash /> </button>
                             <hr />
                         </div>
                     )
@@ -23,16 +34,6 @@ const Note = props => {
         </div>
     )
 }
-const handleDelete = (item) => {
-    
-    if (window.confirm("Delete the item?")) {
-        //Logic to delete the item
-        var element = document.getElementById(item);
-        element.parentNode.removeChild(element);
-    }
-   
 
 
-
-}
 export default Note
